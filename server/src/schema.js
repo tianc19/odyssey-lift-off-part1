6ec6161b552e65ee4a14f048733f641d6793a70b
tianc19 <tianc.19@gmail.com>
2022-06-13 00:11:09 -0700

    first commit

---
+++ server/src/schema.js
@@ -0,0 +1,26 @@
+const {gql} = require('apollo-server');
+
+
+const typeDefs = gql`
+    type Query {
+        tracksForHome: [Track!]!
+    }
+
+    "A group of modules that teaches about a specific topic"
+    type Track {
+        id: ID!
+        title: String!
+        author: Author!
+        thumbnail: String
+        length: Int
+        modulesCount: Int
+    }
+
+    type Author {
+        id: ID!
+        name: String!
+        photo: String
+    }
+`;
+
+module.exports = typeDefs;
